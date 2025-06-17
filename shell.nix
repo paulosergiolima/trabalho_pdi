b7ed60531dd3259cbb44e0ee5265783ffdde307d
kinipk <cloverfieldor@gmail.com>
2025-06-17 18:59:58 -0300

    cross compiling

--- shell.nix
+++ shell.nix
@@ -1,17 +1,22 @@
 { pkgs ? import <nixpkgs> { }, lib ? pkgs.lib }:
 
+let
+  # Lê o rust-toolchain.toml para fixar canal e componentes
+  overrides = builtins.fromTOML (builtins.readFile ./rust-toolchain.toml);
+in
 pkgs.mkShell rec {
-  name = "rust-env";
-
+  name = "rustup-env";
   nativeBuildInputs = [ pkgs.pkg-config ];
   buildInputs = with pkgs; [
-    rustc
-    cargo
     clang
+    llvmPackages.bintools
+    pkg-config
+
+    # Dependências nativas que você já tinha
     openssl
     glib
     atk
-    gtk3                 # inclui gdk, gtk e dependências como gdk-pixbuf, pango, cairo
+    gtk3
     gdk-pixbuf
     pango
     cairo
@@ -24,12 +29,19 @@ pkgs.mkShell rec {
     dbus
     gsettings-desktop-schemas
     gnome-settings-daemon
+
+    rustup
   ];
 
-  # Configura corretamente LD_LIBRARY_PATH para Rust rodar com bibliotecas nativas
   shellHook = ''
+    export PATH=$PATH:${CARGO_HOME:-~/.cargo}/bin
+    export PATH=$PATH:${RUSTUP_HOME:-/.rustup}/toolchains/${overrides.toolchain.channel}-x86_64-unknown-linux-gnu/bin
+
     export LD_LIBRARY_PATH=${lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH
+
     export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
   '';
+
+  RUSTC_VERSION = overrides.toolchain.channel;
 }
 
